5a6a87a986f7932d8e4e6240bd8c9486047bcdf2
Salu VS <salu@focaloid.com>
2016-12-27 11:19:55 +0530

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,58 @@
+import { NgModule, ErrorHandler } from '@angular/core';
+import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
+import { MyApp } from './app.component';
+
+// Importing Pages
+import { HomePage } from '../pages/home/home';
+import { LoginPage } from '../pages/login/login';
+import { ResetPasswordPage } from '../pages/reset-password/reset-password';
+import { SignupPage } from '../pages/signup/signup';
+
+// Importing Providers
+import { AuthData } from '../providers/auth-data';
+
+// Importing AF2 Module
+
+import { AngularFireModule, AuthProviders, AuthMethods } from 'angularfire2';
+
+// AF2 Settings
+const firebaseConfig = {
+  apiKey: "AIzaSyALKfevapBOYK202f6k5mPPfMrT1MHDv5A",
+    authDomain: "bill-tracker-e5746.firebaseapp.com",
+    databaseURL: "https://bill-tracker-e5746.firebaseio.com",
+    storageBucket: "bill-tracker-e5746.appspot.com",
+    messagingSenderId: "508248799540"
+};
+
+const myFirebaseAuthConfig = {
+  provider: AuthProviders.Password,
+  method: AuthMethods.Password
+}
+
+
+@NgModule({
+  declarations: [
+    MyApp,
+    HomePage,
+    LoginPage,
+    ResetPasswordPage,
+    SignupPage
+  ],
+  imports: [
+    IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(firebaseConfig, myFirebaseAuthConfig)
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    HomePage,
+    LoginPage,
+    ResetPasswordPage,
+    SignupPage
+  ],
+  providers: [
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    AuthData
+  ]
+})
+export class AppModule {}
